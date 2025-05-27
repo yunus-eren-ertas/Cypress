a6f9ead4bbbb346c98c42a251056baf88494f8a0
yunus-eren-ertas <r0970901@student.thomasmore.be>
2025-05-27 18:03:21 +0200

    psuh

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "qgf6zd",
   e2e: {
-    projectId: "qgf6zd",
+    specPattern: "e2e/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: "support/e2e.js",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
